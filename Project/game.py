094edde5e7fe5d777a7d5378b17707934fb7bc01
garyli2198 <garyli2198@gmail.com>
2016-12-02 15:55:24 -0800

    added player functions

--- Project/game.py
+++ Project/game.py
@@ -1,5 +1,6 @@
 # Bart Adventure Game.
 class Player(object):
+    backpack = []
 	def __init__(self, name, place):
 		self.name = name
 		self.place = place
@@ -10,36 +11,84 @@ class Player(object):
 	def go_to(self, location):
 		""" Go to a location
 		"""
-        return None
+        destination_place = self.place.get_neighbor(location)
+        if destination_place.locked:
+            print(destination_place.name, 'is locked! Go look for a key to unlock it')
+        else:
+            self.place = destination_place
+        print('You are at', self.place.name)
         
     def ask(self, person):
         """ Ask the person questions """
-        return None
+        if type(person) != str:
+            print("Mentor has to be a string")
+        elif person not in self.place.characters:
+            print(person, "is not here.")
+        else:
+            print(person, "responds: ", self.place.characters[person].response(self))
 
 	def talk_to(self, person):
 		""" Talk to person if person is at player's current place.
 		"""
-        return None
+        if type(person) != str:
+            print('Person has to be a string.')
+        elif person not in self.place.characters:
+            print(person, 'is not here.')
+        else:
+            print(person, 'says: ', self.place.characters[person].talk())
+
 
 	def check_backpack(self):
 		""" Prints out all the items in your backpack.
 		"""
-        return None
+        print('In your backpack:')
+        if not self.backpack:
+            print('    there is nothing.')
+        else:
+            for item in self.backpack:
+                print('   ', item.name, '-', item.description)
+        return [item.name for item in self.backpack]
+
 
 	def take(self, thing):
 		""" Take a thing if at player's current place.
 		"""
-        return None
+        if type(thing) != str:
+            print('Thing should be a string.')
+        elif thing not in self.place.things:
+            print(thing, 'is not here.')
+        else:
+            taken = self.place.take(thing)
+            print(self.name, 'takes the', taken.name)
+            self.backpack.append(taken)
 
     def pick_up(self, friend):
         """ Pick up a friend at player's current place.
         """
-        return None
+        if type(friend) != str:
+            print("Friend should be a string.")
+        elif friend is not self.place.friend.name:
+            print(friend, " is not here.")
+        else:
+            print(self.name, "picks up", self.place.friend.name)
+            self.backpack.append(self.place.friend)
+            self.place.friend = None
 
 	def unlock(self, place):
 		""" Unlocks player's current place, if place is locked and player has ticket.
 		"""
-        return None
+        if type(place) != str:
+            print("Place must be a string")
+            return
+        key = None
+        for item in self.backpack:
+            if type(item) == Key:
+                key = item
+        if not key:
+            print(place, " can't be unlocked without a key!")
+        else:
+            place_to_unlock = self.places.get_neighbor(place)
+            key.use(place_to_unlock)
 
 #requirement, message, bad 
 #player can only ask one question, mentor responds with one answer depending on item in backpack
